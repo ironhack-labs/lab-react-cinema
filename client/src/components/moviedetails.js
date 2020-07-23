ed671f455ad1f73b9c25a6b3cf1a22006e27e917
Daniel Perz <dpere316@fiu.edu>
2020-07-23 17:19:43 -0400

    done

---
+++ client/src/components/moviedetails.js
@@ -0,0 +1,31 @@
+import React, {useState,useEffect}  from 'react';
+import Axios from 'axios';
+
+
+
+const Moviedetails =  (props) => {
+
+   const [movie,setMovie] = useState({})
+
+   useEffect(() => {
+    async function fetchData() {
+      
+      const res = await Axios.get(`http://localhost:5000/movie/${props.match.params.id}`);
+
+      console.log(res)
+      setMovie(res.data)
+    }
+    fetchData();
+  }, [props]); // Or [] if effect doesn't need props or state
+  
+
+    return (
+        <div>
+            <img src={movie.image} alt={movie.title}/>
+            <br/>
+            {movie.title}
+        </div>
+    );
+};
+
+export default Moviedetails;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res) => {
   console.log('called');
   res.json({ message: 'index' });
 });
+
+router.get('/get-movies',(req, res) => {
+  Movies.find().then(movies =>{
+    res.json(movies)
+  })
+});
+
+router.get('/movie/:id',(req, res) => {
+  Movies.findById(req.params.id).then(movie =>{
+    res.json(movie)
+  })
+});
+
+router.post('/movies/new',(req, res) => {
+  Movies.create(req.body).then(()=>{
+    res.json("added movies")
+  })
+  });
+
+
+
 
 module.exports = router;
