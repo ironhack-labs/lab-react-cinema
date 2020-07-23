4473203caa3d4e600cfacc1eb0d476aed9d944c3
Roy Gutierrez <roymg29@gmail.com>
2020-07-23 11:48:40 -0400

    trying this out

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import Home from './components/Home';
-
+import React, { Component } from "react";
+import Home from "./components/Home";
+import Movies from "./components/movies.js";
+import { Switch, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Home />
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+
+          <Route exact path="/movies" render={() => <Movies />} />
+        </Switch>
       </div>
     );
   }
---
+++ client/src/components/movies.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class movies extends Component {
+  render() {
+    return <div>Movies</div>;
+  }
+}
+
+export default movies;
